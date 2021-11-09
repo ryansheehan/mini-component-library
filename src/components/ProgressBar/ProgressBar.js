fd4974d9d473bff08dfaa453a9a1a1913498fcef
ryan <rsheehan@gmail.com>
2021-11-09 16:00:49 -0600

    initial progress bar attempt

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,43 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  const Empty = size === 'small' ? EmptySmall : size === 'medium' ? EmptyMedium : EmptyLarge;
+
+  return (
+    <Empty role="progressBar" aria-valuenow={value} aria-valuemax="100" aria-valuemin="0">
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <FillBar value={value} />      
+    </Empty>
+  );
 };
+
+const EmptyBase = styled.div`
+  width: 100%;
+  height: 8px;
+  position: relative;
+  border-radius: 4px;
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  overflow: hidden;
+`;
+
+const EmptySmall = styled(EmptyBase)``;
+
+const EmptyMedium = styled(EmptyBase)`
+  height: 12px;
+`;
+
+const EmptyLarge = styled(EmptyBase)`
+  height: 24px;
+  border-radius: 8px;
+  padding: 4px;
+`;
+
+const FillBar = styled.div`    
+  height: 100%;
+  width: ${({value}) => `${value}%`};
+  border-radius: ${({value}) => value <= 98 ? '4px 0px 0px 4px' : '4px'};
+  background-color: ${COLORS.primary};
+`;
 
 export default ProgressBar;
