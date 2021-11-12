bd5d68675fa7e1c132c3b1653891620367d0b6be
ryan <rsheehan@gmail.com>
2021-11-12 15:47:55 -0600

    initial input implementation

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,6 +6,21 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+const STYLES = {
+  small: {
+    iconSize: 16,
+    height: '1.125rem',
+    fontSize: '0.875rem',
+    underlineSize: '1px',
+  },
+  large: {
+    iconSize: 24,
+    height: '1.25rem',
+    fontSize: '1.125rem',
+    underlineSize: '2px',
+  }
+}
+
 const IconInput = ({
   label,
   icon,
@@ -13,7 +28,61 @@ const IconInput = ({
   size,
   placeholder,
 }) => {
-  return 'TODO';
+  const config = STYLES[size];
+
+  return (
+    <Wrapper style={{
+      '--width': `${width}px`,      
+      '--borderSize': config.underlineSize,
+    }}>
+      <VisuallyHidden>{label}</VisuallyHidden>
+      <Icon id={icon} size={config.iconSize} strokeWidth={2} />
+      <Input placeholder={placeholder} style={{
+        '--height': config.height,
+        '--fontSize': config.fontSize,
+      }} />
+    </Wrapper>
+  );
 };
 
 export default IconInput;
+
+const Wrapper = styled.div`
+  display: block;  
+  border-bottom: var(--borderSize) solid ${COLORS.Black};
+  display: flex;
+  flex-flow: row nowrap;
+  align-items: flex-end;
+  gap: 8px;
+  width: var(--width);
+  font-family: 'Roboto';  
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+
+  &:focus-within {
+    outline-style:  solid;
+    outline-width:  2px;
+    outline-color: ${COLORS.black};
+    outline-offset: 4px;
+  }
+`;
+
+const Input = styled.input`  
+  height: var(--height);
+  font-size: var(--fontSize);
+  border: none;
+  width: 100%;
+  font-weight: 600;
+
+  &:placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+
+  &:focus {
+    outline: none;  
+  }
+`;
